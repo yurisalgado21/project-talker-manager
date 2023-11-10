fe6b441cb29b3c0a10eecb051c26e446e7c31493
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-11-10 19:20:51 -0300

    exercise 5 done

--- src/middlewares/validateTalk.js
+++ src/middlewares/validateTalk.js
@@ -1,9 +1,6 @@
 const validateRate = (req, res, next) => {
   const { talk } = req.body;
-  if (talk.rate === 0) {
-    return res.status(400)
-      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });      
-  }
+  
   if (!talk.rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
 
   if (!Number.isInteger(talk.rate) || talk.rate < 1 || talk.rate > 5) {
@@ -17,6 +14,10 @@ const validateTalk = (req, res, next) => {
   const { talk } = req.body;
   const isFormatDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
   if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  if (talk.rate === 0) {
+    return res.status(400)
+      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });      
+  }
   if (!talk.watchedAt) {
     return res.status(400)
       .json({ message: 'O campo "watchedAt" é obrigatório' }); 
